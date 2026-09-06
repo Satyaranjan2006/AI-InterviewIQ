316660523192a116bf19e4c979ae4c033fc10e92
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-09-06 23:57:40 +0530

    adding comment

--- servers/config/connectDb.js
+++ servers/config/connectDb.js
@@ -10,3 +10,18 @@ const connectDb=async () => {
     }
 }
 export default connectDb 
+
+
+
+// import mongoose from "mongoose";
+// const connectDb=async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URL)
+//         console.log('Database connected');
+        
+//     } catch (error) {
+//         console.log(`Database error ${error}`);
+        
+//     }
+// }
+// export default connectDb 
